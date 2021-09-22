f9e950d24bb7e194ceee0255c9156d70db0e311b
EvanStrand2022 <818584@mystma.org>
2021-09-22 09:24:08 -0500

    4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #define function
 def topFiveMovies(a, b, c, d, e,):
-    return a + " " + " " + b + " " + c + " " + d + " " + c
+    return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies("Bumblebee", "Sonic The Hedgehog", "Zach Snyders Justice League", "The Dark Knight", "Revenge of the Sith",))
+print(topFiveMovies("Bumblebee", "Sonic The Hedgehog", "Zach Snyders Justice League", "The Dark Knight", "Revenge of the Sith"))
